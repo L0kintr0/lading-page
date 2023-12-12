6271cf37837685b787b036e295fb64dcd385c948
slashmas <junjse4@gmail.com>
2023-12-12 01:13:28 -0300

    agregadas las routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {  RouterLink, RouterOutlet } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from './components/shared/shared.module';
+import { ProductsComponent } from './components/products/products.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HomeComponent,SharedModule,RouterModule],
+  imports: [CommonModule, RouterOutlet,HomeComponent,SharedModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from './components/shared/shared.module';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 export const routes: Routes = [{
-   path:"home",component:HomeComponent
+   path:"home",title:"Home",component:HomeComponent
+},{
+   path:"home/products",title:"Products",component:ProductsComponent
+},{
+   path:"home/about",title:"About",component:AboutComponent
+},{
+   path:"home/contacts",title:"Contact",component:ContactComponent
 }
 ];
